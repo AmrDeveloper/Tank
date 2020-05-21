a6527e6cd9e30c75fd9cfc3c6f8fc023446d242a
AmrDeveloper <amr96@programmer.net>
2020-05-21 16:56:23 +0200

    Create annotation for future useage

--- src/modules/scan.java
+++ src/modules/scan.java
@@ -2,8 +2,10 @@ package modules;
 
 import java.util.Scanner;
 
+@Library
 public class scan {
 
+    @LibraryFunction
     public String scanString() {
         Scanner scr = new Scanner(System.in);
         return scr.next();
--- src/modules/system.java
+++ src/modules/system.java
@@ -2,13 +2,16 @@ package modules;
 
 import java.io.IOException;
 
+@Library
 public class system {
 
+    @LibraryFunction
     public void openTerminal( ) throws IOException {
         Runtime runtime = Runtime.getRuntime();
         runtime.exec("cmd.exe /c start", null);
     }
 
+    @LibraryFunction
     public void testArgs (String name) {
         System.out.println("hello");
     }

25a33c7c4cbdae8389acbd4de0cf7de8a593c691
AmrDeveloper <amr96@programmer.net>
2020-05-25 15:10:10 +0200

    Create Parser Package and json module

--- src/Tank.java
+++ src/Tank.java
@@ -3,7 +3,7 @@ import runtime.TankRuntime;
 public class Tank {
 
     public static void main(String[] args) {
-        String sourceFile = "examples/Str.tank";
+        String sourceFile = "examples/JsonTest.tank";
         TankRuntime.runTankFile(sourceFile);
         //TankRuntime.runTankTerminal();
     }
---
+++ src/modules/parsers/json.java
@@ -0,0 +1,35 @@
+package modules.parsers;
+
+import modules.Library;
+import modules.LibraryFunction;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+@Library
+public class json {
+
+    @LibraryFunction
+    public JSONObject jsonObject(String source) {
+        return new JSONObject(source);
+    }
+
+    @LibraryFunction
+    public JSONArray jsonArray(String source) {
+        return new JSONArray(source);
+    }
+
+    @LibraryFunction
+    public JSONObject jsonArrayObject(JSONArray jsonArray, Double index) {
+        return jsonArray.getJSONObject(index.intValue());
+    }
+
+    @LibraryFunction
+    public Integer jsonArrayLength(JSONArray jsonArray) {
+        return jsonArray.length();
+    }
+
+    @LibraryFunction
+    public Object jsonObjectValue(JSONObject jsonObject, String key) {
+        return jsonObject.get(key);
+    }
+}

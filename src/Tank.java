0e3dbae35b98cf66eec21d469276a9ea26eb5c4d
AmrDeveloper <amr96@programmer.net>
2020-05-25 13:30:45 +0200

    Update String modules

--- src/Tank.java
+++ src/Tank.java
@@ -3,7 +3,7 @@ import runtime.TankRuntime;
 public class Tank {
 
     public static void main(String[] args) {
-        String sourceFile = "examples/Array.tank";
+        String sourceFile = "examples/Str.tank";
         TankRuntime.runTankFile(sourceFile);
         //TankRuntime.runTankTerminal();
     }
--- src/modules/string.java
+++ src/modules/string.java
@@ -1,5 +1,7 @@
 package modules;
 
+import ast.Array;
+
 @Library
 public class string {
 
@@ -11,5 +13,85 @@ public class string {
     @LibraryFunction
     public boolean strMatch(String str, String regex) {
         return str.matches(regex);
+    }
+
+    @LibraryFunction
+    public String strConcat(String first, String second) {
+         return first.concat(second);
+    }
+
+    @LibraryFunction
+    public boolean strContains(String str, String sequence) {
+        return str.contains(sequence);
+    }
+
+    @LibraryFunction
+    public String strTrim(String str) {
+        return str.trim();
+    }
+
+    @LibraryFunction
+    public String strUpperCase(String str) {
+        return str.toUpperCase();
+    }
+
+    @LibraryFunction
+    public String strLowerCase(String str) {
+        return str.toLowerCase();
+    }
+
+    @LibraryFunction
+    public boolean strStartWith(String str, String sequence) {
+        return str.startsWith(sequence);
+    }
+
+    @LibraryFunction
+    public boolean strEndWith(String str, String sequence) {
+       return str.endsWith(sequence);
+    }
+
+    @LibraryFunction
+    public Integer strIndexOf(String str, String sequence) {
+        return str.indexOf(sequence);
+    }
+
+    @LibraryFunction
+    public Integer strLastIndexOf(String str, String sequence) {
+        return str.lastIndexOf(sequence);
+    }
+
+    @LibraryFunction
+    public String strReplaceFirst(String str, String regex, String replacement) {
+        return str.replaceFirst(regex, replacement);
+    }
+
+    @LibraryFunction
+    public String strReplaceAll(String str, String regex, String replacement) {
+        return str.replaceAll(regex, replacement);
+    }
+
+    @LibraryFunction
+    public Character strCharAt (String str, Double index) {
+        return str.charAt(index.intValue());
+    }
+
+    @LibraryFunction
+    public String strSubstring (String str, Double start, Double end) {
+        return str.substring(start.intValue(), end.intValue());
+    }
+
+    @LibraryFunction
+    public boolean strIsEmpty(String str) {
+        return str.isEmpty();
+    }
+
+    @LibraryFunction
+    public Array strSplit(String str, String regex) {
+        return new Array(str.split(regex));
+    }
+
+    @LibraryFunction
+    public Integer strCompareTo(String first, String second) {
+        return first.compareTo(second);
     }
 }

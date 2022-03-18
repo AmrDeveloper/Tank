fb5b3442ab2d719719095625ae894de77ef03170
AmrDeveloper <amr96@programmer.net>
2022-03-18 13:32:11 +0200

    Introduce Tank Options to the start point to use it later in CLI

--- src/Tank.java
+++ src/Tank.java
@@ -4,10 +4,29 @@ import java.io.IOException;
 
 public class Tank {
 
+    enum TankOptions {
+        OPTION_RUN_FILE,
+        OPTION_RUN_REPL,
+        OPTION_RUN_LINT,
+    }
+
+    private static final TankOptions currentOption = TankOptions.OPTION_RUN_FILE;
+
     public static void main(String[] args) throws IOException {
         String sourceFile = "examples/InfixPrefix.tank";
-        //TankRuntime.checkFileCodeStyle(sourceFile);
-        TankRuntime.runTankFile(sourceFile);
-        //TankRuntime.runTankTerminal();
+        switch (currentOption) {
+            case OPTION_RUN_FILE: {
+                TankRuntime.runTankFile(sourceFile);
+                break;
+            }
+            case OPTION_RUN_REPL: {
+                TankRuntime.runTankTerminal();
+                break;
+            }
+            case OPTION_RUN_LINT: {
+                TankRuntime.checkFileCodeStyle(sourceFile);
+                break;
+            }
+        }
     }
 }
